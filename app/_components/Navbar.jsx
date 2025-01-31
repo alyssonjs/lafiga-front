131b9317b1ed3429f6898d126d25fd4571589e77
Matthaeus <matthaeusbm@gmail.com>
2025-01-31 11:26:36 -0300

    fix: import in Navbar component

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../styles/NavBar.module.css';
+import styles from '../_styles/NavBar.module.css';
 import Image from 'next/image';
 
 const Navbar = () => {
