f92b70437c79d2f36a70e93a5893eeef3ee5761e
alyssonjs <alysson.soares@hotmail.com.br>
2025-04-27 00:33:28 -0300

    update style

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,7 +22,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className={pixel.className}>
+      <body className={start.className} style={{fontSize: '10px'}}>
         <AuthProvider>
           <Navbar />
           <main>{children}</main>
